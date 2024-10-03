686cf4a5aa566a8ce0c53965340fc78ebe88c470
Jannchie <panjianqi@preferred.jp>
2024-10-03 23:46:14 +0800

    :sparkles: feat(commit): add rich print for missing commit message

--- tgit/commit.py
+++ tgit/commit.py
@@ -7,6 +7,7 @@ from typing import Optional
 import git
 from openai import OpenAI
 from pydantic import BaseModel
+from rich import print
 
 from tgit.settings import settings
 from tgit.utils import get_commit_command, run_command, type_emojis
@@ -23,7 +24,11 @@ def define_commit_parser(subparsers: argparse._SubParsersAction):
         commit_type.append(data.get("type"))
 
     parser_commit = subparsers.add_parser("commit", help="commit changes following the conventional commit format")
-    parser_commit.add_argument("message", help="commit message", type=str, nargs="*")
+    parser_commit.add_argument(
+        "message",
+        help="commit message, the first word should be the type, if the message is more than two parts, the second part should be the scope",
+        nargs="*",
+    )
     parser_commit.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
     parser_commit.add_argument("-e", "--emoji", action="store_true", help="use emojis")
     parser_commit.add_argument("-b", "--breaking", action="store_true", help="breaking change")
@@ -79,6 +84,9 @@ def handle_commit(args: CommitArgs):
         command = get_ai_command()
     else:
         messages = args.message
+        if len(messages) == 0:
+            print("Please provide a commit message, or use --ai to generate by AI")
+            return
         commit_type = messages[0]
         if len(messages) > 2:
             commit_scope = messages[1]
